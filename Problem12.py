22b800262f2fe809a3a085daa572c2a69eae94eb
Kevin <dingkevin@gmail.com>
2014-12-24 15:20:00 -0600

    Add problem 12, 13

--- Problem12.py
+++ Problem12.py
@@ -1,15 +1,14 @@
 def numOfFactors(n):
-    factors = []
+    factors = 0
     for i in range(1, int(n+1)):
         if n%i == 0:
-            factors.append(i)
-    return len(factors)
+            factors += 1
+    return factors
         
-triangleNum = []
-i=0
+i = 0
 while True:
     i += 1
-    x = i*(i+1)/2
+    x = i*(i+1)//2
     if numOfFactors(x) > 500:
         print(x)
         break
